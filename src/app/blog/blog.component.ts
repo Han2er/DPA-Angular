23bbaa11ff8a5fb94c98a2969c5c7f59a782cf35
han2er <han2er@live.com>
2023-07-06 21:11:22 +0400

    angular migration backup

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
-  styleUrls: ['./blog.component.scss']
+  styleUrls: ['./blog.component.scss'],
 })
 export class BlogComponent {
+  blogs:
+    | [
+        { id: 1; name: 'დააასდ'; prophecy: '11' },
+        { id: 2; name: 'sas'; prophecy: '11' },
+        { id: 3; name: 'qqwq'; prophecy: '11' }
+      ]
+    | undefined;
 
+  ngOnInit(): void {
+    // this.blogs
+  }
 }
